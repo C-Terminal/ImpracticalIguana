3a4238f9717de494701d7f82ce802769ff80db94
C-Terminal <didactictraveller@skiff.org>
2023-07-02 22:29:41 -0400

    Palindrome and paligram detecting code completed

--- Segment 01/broke_bar_chart/letter_freq_bar_chart.py
+++ Segment 01/broke_bar_chart/letter_freq_bar_chart.py
@@ -1,11 +1,11 @@
-from pprint import pprint
+# from pprint import pprint
+from collections import defaultdict
+
 import string
 
 sentence = "He fumbled in the darkness " \
            "looking for the light switch, but when he finally " \
            "found it there was someone already there.Toddlers feeding raccoons"
-
-from collections import defaultdict
 
 word_freq_dict = defaultdict(list)
 
@@ -15,10 +15,5 @@ for letter in string.ascii_lowercase:
 print(word_freq_dict)
 
 for letter in sentence:
-    if letter in word_freq_dict: word_freq_dict[letter].append(letter)
-
-# formatted_data = {
-#     key: len('#' * word_freq_dict.key), value in word_freq_dict.items()
-# }
-# pprint(word_freq_dict.items())
-
+    if letter in word_freq_dict:
+        word_freq_dict[letter].append(letter)
